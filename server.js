e1daf5dfbcc22bfd43c4deaeb22ce45b091b1ec9
Alden Ling <aldenling@Aldens-MacBook-Pro.local>
2017-02-09 12:53:33 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+// default directory express use for templates is 'views'
+app.set('view engine', 'hbs');
+
+// create middleware (stacks for express)
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    // call next to have middleware to move to next
+    next();
+});
+
+// app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: 'Alden',
+    //     likes: [
+    //         'Tennis',
+    //         'Hiking'
+    //     ]
+    // });
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to some website',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+       errorMessage: 'Unable to respond'
+   });
+});
+
+
+app.listen(3000, () => {
+    console.log('Sever is up on port 3000.');
+});
